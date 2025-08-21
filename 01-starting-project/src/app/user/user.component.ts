fdaad38011bbf4f4ef13e2cfe67addb63752f247
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-08-21 17:06:13 -0300

    1-Starting-Project

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
