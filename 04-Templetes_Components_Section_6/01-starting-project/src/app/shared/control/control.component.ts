c6a4d77287a7e2bba57b5acb56f651edde58c82a
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-10-02 12:25:24 -0300

    ContentChild

--- 04-Templetes_Components_Section_6/01-starting-project/src/app/shared/control/control.component.ts
+++ 04-Templetes_Components_Section_6/01-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ContentChild, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -19,9 +19,11 @@ export class ControlComponent {
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
-
+  //@ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
+  private control = contentChild.required<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input')
   onCLick() {
     console.log('clicked');
     console.log(this.el);
+    console.log(this.control());
   }
 }
