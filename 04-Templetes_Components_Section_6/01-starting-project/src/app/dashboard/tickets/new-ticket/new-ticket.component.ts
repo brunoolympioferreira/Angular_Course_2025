ddb66be9f0bca81d372698946d7eb65c4b791156
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-10-08 08:22:10 -0300

    Inputs e Signals

--- 04-Templetes_Components_Section_6/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 04-Templetes_Components_Section_6/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -14,7 +14,7 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
   @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   //@Output() add = new EventEmitter();
-  add = output<{ title: string, text: string }>()
+  add = output<{ title: string; text: string }>();
 
   ngOnInit() {
     console.log('ON INIT');
@@ -27,7 +27,7 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
   }
 
   onSubmit(title: string, ticketText: string) {
-    this.add.emit({ title, text: ticketText });
+    this.add.emit({ title: title, text: ticketText });
     this.form?.nativeElement.reset();
   }
 }
