c88da9624e7207a0ebe74c5b452913c6d7803113
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-10-24 08:01:17 -0300

    Host Directive part 2

--- 05-Directives/01-starting-project-directives/src/app/app.component.ts
+++ 05-Directives/01-starting-project-directives/src/app/app.component.ts
@@ -5,14 +5,13 @@ import { LearningResourcesComponent } from './learning-resources/learning-resour
 import { AuthService } from './auth/auth.service';
 import { NgIf } from '@angular/common';
 import { AuthDirective } from "./auth/auth.directive";
-import { LogDirective } from "./log.directive";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, AuthDirective, LogDirective],
+  imports: [AuthComponent, LearningResourcesComponent, AuthDirective],
 })
 export class AppComponent {
   private authService = inject(AuthService);
